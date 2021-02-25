b301f46b308f449a77c182ff23fb039c69b40813
Eric Ngo <engo1207@gmail.com>
2021-02-25 10:27:42 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,18 @@
 function initMap() {
 	// add your code here
+
+	L.mapquest.key = 'XiaB1KIvun2TFVFRGAKhkqCuh9UEjF41';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.8785248, -117.2380747],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.8785248, -117.2380747], {
+		icon: L.mapquest.icons.marker(), 
+		draggable: false
+	}).addTo(map);
 }
